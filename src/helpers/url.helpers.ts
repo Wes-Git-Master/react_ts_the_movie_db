0e174237b1305ec723e26100dddefdacaae28913
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-06-20 23:15:49 +0300

    main

--- src/helpers/url.helpers.ts
+++
@@ -1,3 +0,0 @@
-export const getAuthUrl = (requestToken: string, redirectTo: string = 'http://localhost:3000/authenticated'): string => {
-    return `https://www.themoviedb.org/authenticate/${requestToken}?redirect_to=${redirectTo}`;
-};
--- src/interfaces/IMoviesAxiosResponse.ts
+++
@@ -1,8 +0,0 @@
-import {IMovie} from "./IMovie";
-
-export interface IMoviesAxiosResponse{
-    page: number,
-    results: IMovie[],
-    total_pages: number,
-    total_results: number
-}
