3ba4c6ef7c4ab0f18f4f5f7bad0e0f749c552afa
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-06-21 01:12:06 +0300

    main

--- src/hooks/reduxHooks/redux.type.hooks.ts
+++
@@ -1,5 +0,0 @@
-import {useDispatch, useSelector} from "react-redux";
-import {store} from "../../redux/Store";
-
-export const useAppDispatch = useDispatch.withTypes<typeof store.dispatch>();
-export const useAppSelector = useSelector.withTypes<ReturnType<typeof store.getState>>();
